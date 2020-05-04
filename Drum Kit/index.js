2fee68c56d72f992e041b628e3a06bfb26c20ba5
I336190 <martin.strnad@sap.com>
2020-05-04 12:08:54 +0200

    Section 12: Advanced Javascript and DOM Manipulation: Adding Event Listeners to a Button - HandleClick() function

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,4 +1,9 @@
 
+/* Incorrect call of handleClick function:
+  document.querySelector("button").addEventListener("click", handleClick());
+  --> if the method in the EventListener has parenthesis "handleClick()"" , 
+        then it will be called immediately and not wait until Event "click" happened  
+*/
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick() {
