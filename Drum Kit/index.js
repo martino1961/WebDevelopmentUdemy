5f007f33073849d07e426e5c428c3b6ae5f18c56
I336190 <martin.strnad@sap.com>
2020-05-06 11:04:02 +0200

    Section 12: Advanced Javascript and DOM Manipulation: Drum Kit with Animation

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -76,12 +76,27 @@ function makeSound(key) {
 }
 
 function buttonAnimation(currentKey) {
-    
+    //HTML:  <button class="w drum">w</button>
+    //in CSS is class "pressed": .pressed { box-shadow: 0 3px 4px 0 #DBEDF3; opacity: 0.5; } 
+    //if we Add/Remove class "pressed" to <button> we will see Animation
+
+    //Animation: 
+    // 1) add    class "pressed"
+    // 2) remove class "pressed"
+    //To see the animation we must have some timeout between (1) and (2)
+
     //currentKey is the name of class --> querySelector(".class")
     //                                --> concatenate "." and currentKey
     var activeButton = document.querySelector("." + currentKey);
 
-    //HTML:  <button class="w drum">w</button>
-    // add class "pressed" to button
-    activeButton.classList.add("pressed");
+    //Animation (1): Add class "pressed":
+     activeButton.classList.add("pressed");
+
+    //Animation (2): Remove class "pressed"
+    //Set Timeout and then remove "pressed" class
+    setTimeout(function() {
+        //Anonymous function
+       activeButton.classList.remove("pressed"); 
+    }, 250); //Timeout in msec
+
 }
