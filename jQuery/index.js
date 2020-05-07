da3c5d6322a25e78c51811d4af421d68f539ec11
I336190 <martin.strnad@sap.com>
2020-05-07 12:23:13 +0200

    Section 13: jQuery - link to jQuery at the end of <body> exactly before link to JS Code

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,10 +1,4 @@
-//Call of document.ready() function in case the link to jQuery is in the <header>
-//document.ready() waits until the whole body is uploaded
-//  
-//Call of document.ready() is not needed if the link to jQuery is placed ad the end of <body>
-$(document).ready(function() {
-    $("h1").css("color", "red");
-});
 
+$("h1").css("color", "red");
 
 
