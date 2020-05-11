332bbfe2cb88e987ae85c3a3673fe4abb1d6432f
I336190 <martin.strnad@sap.com>
2020-05-11 17:39:30 +0200

    Section 14: jQuery: 189. Manipulating Styles with jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,18 @@
 
-$("h1").css("color", "red");
+//$("h1").css("color", "red");  //Set <h1> css color to red
+/* method .css( "oneParameter" ) - Get value of "oneParameter" */
+//console.log($("h1").css("font-size"));
+/* method .css( "oneParameter", "secondParameter" ) - Set value "secondParameter" of "oneParameter" */
+//$("h1").css("font-size", "5rem");
 
+/* Add Class "big-title to <h1>*/
+//$("h1").addClass("big-title");
+
+/* Adding more Classes: separate classes with ' ' */
+$("h1").addClass("big-title margin-50");
+
+//Check if Element has Class
+console.log($("h1").hasClass("margin-50"));  //.hasClass retruns true/false
+
+$("button");   //document.querySelectorAll("button");
 
