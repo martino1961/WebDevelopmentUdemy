25c3047f35ba98b5304d716b58410a236f4f7c72
I336190 <martin.strnad@sap.com>
2020-05-12 18:33:20 +0200

    Section 14: jQuery: 192. Add Event Listener using method .on()

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,38 +1,5 @@
-//<h1> Add Event Listener for Mouse Click
-/* $("h1").click(function() {
+//Event Listener using method ".on"
+//List of Events: https://developer.mozilla.org/en-US/docs/Web/Events
+$("h1").on("mouseover", function() {
     $("h1").css("color", "purple");
-}); */
-
-//JS: Change Color of <h1> by Click of whichever of 5 buttons
-// for (var i = 0; i<5; i++) {
-//     document.querySelectorAll("button")[i].addEventListener("click", function() {
-//         document.querySelector("h1").style.color = "purple";
-//     }) 
-// }
-
-//jQuery: Change Color of <h1> by Click of whichever of 5 buttons
-// $("button").click(function() {
-//     $("h1").css("color", "purple");
-// })
-
-//JS: Key pressed
-// document.querySelector("input").addEventListener("keydown", function(event) {
-//     console.log(event.key);
-// });
-
-//jQuery: Key pressed
-// $("input").keydown(function(event) {
-//     console.log(event.key);
-// });
-
-// Challenge: Show pressed key in <h1>
-//Solution 1: $("body") - with jQuery method '.html':
-// $("body").keydown(function(event) {
-//     $("h1").html(event.key);
-// })
-
-// Challenge: Show pressed key in <h1>
-//Solution 2: $(document) - with jQuery method '.text':
-$(document).keydown(function(event) {
-    $("h1").text(event.key);
-})
+});
