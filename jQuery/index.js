c193105d5f12696f0490913030c9462f5206507b
I336190 <martin.strnad@sap.com>
2020-05-12 17:51:58 +0200

    Section 14: jQuery 192. Adding Event Listener for Mouse Click of 5 buttons

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,16 @@
 //<h1> Add Event Listener for Mouse Click
-$("h1").click(function() {
+/* $("h1").click(function() {
     $("h1").css("color", "purple");
-});
+}); */
+
+//JS: Change Color of <h1> by Click of whichever of 5 buttons
+// for (var i = 0; i<5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple";
+//     }) 
+// }
+
+//jQuery: Change Color of <h1> by Click of whichever of 5 buttons
+$("button").click(function() {
+    $("h1").css("color", "purple");
+})
