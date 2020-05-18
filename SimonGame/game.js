0139a8d0a7070b33cb910830ce4b93e82142ab43
I336190 <martin.strnad@sap.com>
2020-05-18 16:31:40 +0200

    Sec 15

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -2,12 +2,17 @@
 //SG1: HTML - add links to jQuery and JS
 //SG2: JS - Create A New Pattern
 //SG3: JS - Step 3 - Show the Sequence to the User with Animations and Sounds
+//SG4: JS - Step 4 - Check Which Button is Pressed
+
 
 //SG Step 2 (SG2) - Create a New patern
 //SG2.3 New array buttonColours
 var buttonColours = ["red", "blue", "green", "yellow"];
 //SG2.5. New array gamePattern
 var gamePattern = [];
+//SG4.3 Create a new empty array with the name userClickedPattern
+var userClickedPattern = [];
+
 
 //SG2.1 Create a new function nextSequence()
 function nextSequence() {
@@ -25,5 +30,21 @@ function nextSequence() {
   //SG3.3 Play sound of the selected button
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
-  
+
 }
+
+//SG4.1 Use jQuery to detect when any of the buttons are clicked and 
+//      trigger a handler function
+$(".btn").click(function() {
+    //SG4.2 Id of clicked button
+      var userChosenColour = $(this).attr("id");
+    
+    //SG4.4 Add the contents of userChosenColour to the end of userClickedPattern[]  
+    userClickedPattern.push(userChosenColour);
+    
+    //console.log(userClickedPattern);
+    
+    });
+
+
+
