993cafe59bfaa2e34202ff8e617828749cb48938
I336190 <martin.strnad@sap.com>
2020-05-18 15:58:51 +0200

    Section 15: Boss Level Challenge 2 - The Simon Game: Step 2 - Create A New Pattern

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -1,6 +1,17 @@
-//SG Step 1 (SG1) - Add JS and jQuery to SG
-//(SG1.1) - Check link to JS works
-alert("Hello!");
+//Simon Game - shortcut SG
 
-//(SG1.2) - Check link to jQuery works
-$("h1").css("color", "green");
+//SG Step 2 (SG2) - Create a New patern
+//SG2.3 New array buttonColours
+var buttonColours = ["red", "blue", "green", "yellow"];
+//SG2.5. New array gamePattern
+var gamePattern = [];
+
+//SG2.1 Create a new function nextSequence()
+function nextSequence() {
+
+  //SG2.2 Random numbers: 0 - 3
+  var randomNumber = Math.floor(Math.random() * 4);
+  //SG2.4 Save a random chosen color
+  var randomChosenColour = buttonColours[randomNumber];
+
+}
