d184ac573f1e20b0d712bff2906b40df01fe6c2b
I336190 <martin.strnad@sap.com>
2020-05-18 16:05:55 +0200

    Section 15: Boss Level Challenge 2 - The Simon Game: Step 2 - Create A New Pattern - Add random chosen color to the end of array gamePattern

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -1,4 +1,7 @@
 //Simon Game - shortcut SG
+//SG1: HTML - add links to jQuery and JS
+//SG2: JS - Create A New Pattern
+//SG3: JS - Step 3 - Show the Sequence to the User with Animations and Sounds
 
 //SG Step 2 (SG2) - Create a New patern
 //SG2.3 New array buttonColours
@@ -13,5 +16,7 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   //SG2.4 Save a random chosen color
   var randomChosenColour = buttonColours[randomNumber];
+  //SG2.6 Add the new randomChosenColour to the end of the gamePattern[]
+  gamePattern.push(randomChosenColour);
 
 }
