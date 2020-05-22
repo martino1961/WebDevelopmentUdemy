5a5f29d6cbd12e40c62cf58e04f700b6bcd4b40d
I336190 <martin.strnad@sap.com>
2020-05-22 10:48:42 +0200

    Sec18: Node.js: Run index.js directly from PC (not from browser)

---
+++ Node_js/intro-to-node/index.js
@@ -0,0 +1,6 @@
+//Section 18: Node.js: 234. The Power of the Command Line and How to Use Node
+//Run index.js in PC
+console.log("Hello, world!");
+//Run index.js in GitBash:
+//1) cd Directory_where_is_index.js --> change directory to the dir where index.js is located
+//2) node index.js --> displays "Hello World!""
---
+++ Node_js/readme.txt
@@ -0,0 +1,3 @@
+GitBash:
+
+node --version --> shows the version of Node.js - should be used to check if the Node.js was successfully installed
