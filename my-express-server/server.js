5fa1c969baa10cfceacf7f42b6e614eaa5090a6c
I336190 <martin.strnad@sap.com>
2020-05-27 15:39:21 +0200

    Sec19: First server with Express.js - Add callback function to server

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,4 +6,11 @@ const express = require("express");
 const app = express();
 
 //port 3000 - for HTTP request
-app.listen(3000);  //launch in Cmdline: node server.js --> sever will listen --> interrupt server by Ctrl+C
+//launch in Cmdline: node server.js 
+//  --> sever will listen --> interrupt server by Ctrl+C
+//app.listen(3000);  
+
+//Check server by callback function
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+})
