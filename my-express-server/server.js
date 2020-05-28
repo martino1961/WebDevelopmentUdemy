fd86b7f1ee64a640a2c55ff98deac12452e7f6bd
I336190 <martin.strnad@sap.com>
2020-05-28 10:05:48 +0200

    Sec19: 244. Understanding and Working with Routes - send response to GET page

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,25 +1,26 @@
-//Section 19: Express.js with Node.js - 242. Handling Requests and Responses: the GET Request
-
-//1) start server: in CmdLine: node server.js
-//2) in browser: localhost:3000
+//Section 19: Express.js with Node.js - 244. Understanding and Working with Routes
 
 //Include package express
 const express = require("express");
 //Express app
 const app = express();
 
-//Shortcut: 
-// req - for Request
-// res - for Response 
+//GET "/" - GET Homepage (Route)
 app.get("/", function(req, res){
    res.send("<h1>Hello!</h1>");  //Sends "Hello!" to the browser
 });
 
-//port 3000 - for HTTP request
-//launch in Cmdline: node server.js 
-//  --> sever will listen --> interrupt server by Ctrl+C 
+//GET "/contact" page
+app.get("/contact", function(req, res){
+    res.send("Contact me at: strnadm@gmail.com");
+});
 
-//Check server by callback function
+//GET "/about" page
+app.get("/about", function(req, res){
+    res.send("My name is <h1>Martin</h1> and I love <h2>swimming, walking and coding.</h2>");
+});
+
+//port 3000 - for HTTP request
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 })
