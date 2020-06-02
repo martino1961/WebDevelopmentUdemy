5a50ecb403831947e30c04523ed4c63e9800b152
I336190 <martin.strnad@sap.com>
2020-06-02 16:30:47 +0200

    Sec20 - API: 259. How to Parse JSON

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,4 +1,4 @@
-//Section 20 - API: 258. Making GET Requests with the Node HTTPS Module
+//Section 20 - API: 259. How to Parse JSON
 
 const express = require("express");
 const https = require("https");
@@ -6,11 +6,28 @@ const https = require("https");
 const app = express();
 
 app.get("/", function(req, res) {
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Zilina&appid=40b476b839899e1f0d9cab8f04a2c69e&units=metric";
+    //My (free of charge) CurrentWeather API Key: 40b476b839899e1f0d9cab8f04a2c69e
+    const apiKey = "40b476b839899e1f0d9cab8f04a2c69e";
+    const city = "Zilina";
+    const openWeatherPage = "https://api.openweathermap.org/data/2.5/weather";
+    const language = "sk";
+    //URL parameters (separeted by '&') should be in whichever order
+    //const url = "https://api.openweathermap.org/data/2.5/weather?q=Zilina&appid=40b476b839899e1f0d9cab8f04a2c69e&units=metric&lang=sk";
+    const url = openWeatherPage + "?q=" + city + "&lang=" + language + "&units=metric" + "&appid=" + apiKey;
+    //console.log("URL: " + url);
     https.get(url, function(response) {
         console.log("StatusCode " + response.statusCode);
-      //console.log(response);
+
+        //in "data" is response of GET request (by default the data are in JSON format)
+        response.on("data", function(data) {
+            //Data are in HexaDecimal format --> we have to convert them
+            const weatherData = JSON.parse(data);
+            const temperature = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+
+            console.log("Current temperature: " + temperature + " degrees");
+            console.log("Weather description: " + weatherDescription);
+        })
     });
 
     res.send("Server is up and running");
