e0289470784189779213fbd4da4ed19aa16e4302
I336190 <martin.strnad@sap.com>
2020-06-04 20:22:13 +0200

    Sec20: 263. Newsletter-SignUp app: Setting Up the Sign Up Page

--- Newsletter-Signup/apps.js
+++ Newsletter-Signup/apps.js
@@ -1,4 +1,4 @@
-//Sec20: 262. The Mailchimp API - Initial Setup of Server
+//Sec20: 263. Setting Up the Sign Up Page
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -6,8 +6,26 @@ const request = require("request");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true}));
 
+//for addressing css and image folders statically (in "public" folder)
+app.use(express.static("public"));
+
+//Get "/" (app Home Page)
+app.get("/", function(req, res) {
+    //res.send("Server is up and running");
+    res.sendFile(__dirname + "/signup.html");
+})
+
+//Catch SignMeUp button on page "/" (type="submit") via "<form action="/" method="POST">"
+app.post("/", function(req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email);
+})
 
 app.listen(3000, function() {
-    console.log("Server is running on the port 3000.")
+    console.log("Server is running on the port 3000.");
 })
