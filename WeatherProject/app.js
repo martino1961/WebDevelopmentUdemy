fe3c5d344dde3ae7087c8ff98894aa48350d235d
I336190 <martin.strnad@sap.com>
2020-06-04 17:25:06 +0200

    Sec20: API: WeatherProject - added comments

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -9,22 +9,38 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Get "/" (app Home Page)
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+//Catch GoButton (type="submit") via "<form action="/" method="POST">"
 app.post("/", function (req, res) {
-//  console.log("City: " + req.body.cityName);
   const apiKey = "40b476b839899e1f0d9cab8f04a2c69e"; //My (free of charge) CurrentWeather API Key
-  const city = req.body.cityName; //"Zilina";
+  //cityName taken from input field on "/" (app Home Page)
+  const city = req.body.cityName; //Entered cityName on "/" (app Home Page)
   const openWeatherPage = "https://api.openweathermap.org/data/2.5/weather";
-  //const language = "en";
-  const language = "sk";
+  const language = "sk";  //"en"
   const unit = "metric";
+
+  var msg1 = "Current temperature in ";
+  var msg2 = " is ";
+  var msg3 = " degrees Celcius.";
+
+  switch (language) {
+      case "sk":
+          msg1 = "Aktuálna teplota v meste ";
+          msg2 = " je ";
+          msg3 = " stupňov Celzia.";
+          break;
+  
+      default:
+          break;
+    } 
 
   //const url = "https://api.openweathermap.org/data/2.5/weather?q=Zilina&appid=40b476b839899e1f0d9cab8f04a2c69e&units=metric&lang=en";
   const url = openWeatherPage + "?q=" + city + "&lang=" + language + "&units=" + unit + "&appid=" + apiKey;
-
+  //GET to external WebPage
   https.get(url, function (response) {
     console.log("StatusCode " + response.statusCode);
 
@@ -36,28 +52,14 @@ app.post("/", function (req, res) {
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      var msg1 = "Current temperature in ";
-      var msg2 = " is ";
-      var msg3 = " degrees Celcius.";
-
-      switch (language) {
-          case "sk":
-              msg1 = "Aktuálna teplota v meste ";
-              msg2 = " je ";
-              msg3 = " stupňov Celzia.";
-              break;
-      
-          default:
-              break;
-      } 
-
       //Set UTF-8 Encoding (needed for Slovak language)
-      //res.write() needs to be set the Encoding
+      //res.write() needs to be set the Encoding for 'text/html'
       res.set({ 'content-type': 'text/html; charset=utf-8' });
 
       res.write("<p><h1>" + msg1 + city + msg2 + temperature + msg3 + "</h1></p>");
       res.write("<p><h2>" + weatherDescription + "</h2></p>");
       res.write("<img src=" + imageURL + ">");
+      //Send written HTML to browser
       res.send();
     });
   });
